6ffb313bb0f4934e3ed4c24fba9486452d11516e
zhanghuaixiao <zhanghauixaio@163.com>
2024-04-10 13:29:37 +0800

    excel 样式

---
+++ src/main/java/com/seven/excel/CustomizeColumnWidth.java
@@ -0,0 +1,57 @@
+package com.seven.excel;
+
+import com.alibaba.excel.metadata.Head;
+import com.alibaba.excel.metadata.data.WriteCellData;
+import com.alibaba.excel.write.metadata.holder.WriteSheetHolder;
+import com.alibaba.excel.write.metadata.holder.WriteTableHolder;
+import com.alibaba.excel.write.style.column.AbstractColumnWidthStyleStrategy;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+import java.util.List;
+
+public class CustomizeColumnWidth extends AbstractColumnWidthStyleStrategy {
+    //自定义列宽
+    @Override
+    protected void setColumnWidth(WriteSheetHolder writeSheetHolder, List<WriteCellData<?>> list, Cell cell, Head head, Integer integer, Boolean isHead) {
+        // 测试为 COLUMN 宽度定制.
+        if (isHead && cell.getRowIndex() == 2) {
+            int columnWidth = cell.getStringCellValue().getBytes().length;
+            int cellIndex = cell.getColumnIndex();
+            switch (cellIndex) {
+                case 0:
+                case 2:
+                case 3:
+                    columnWidth = 10;
+                    break;
+                case 1:
+                    columnWidth = 12;
+                    break;
+                case 4:
+                    columnWidth = 15;
+                    break;
+                case 5:
+                    columnWidth = 50;
+                    break;
+                default:
+                    break;
+            }
+
+            if (columnWidth > 255) {
+                columnWidth = 255;
+            }
+            writeSheetHolder.getSheet().setColumnWidth(cellIndex, columnWidth * 256);
+        }
+    }
+
+    //自定义行高
+    @Override
+    public void beforeCellCreate(WriteSheetHolder writeSheetHolder, WriteTableHolder writeTableHolder, Row row, Head head, Integer columnIndex, Integer relativeRowIndex, Boolean isHead) {
+        // 设置行高测试
+        int rowIndex = row.getRowNum();
+        System.out.println("当前行: " + rowIndex);
+        short height = 600;
+        row.setHeight(height);
+    }
+}
+
--- src/main/java/com/seven/excel/ExcelUtils.java
+++ src/main/java/com/seven/excel/ExcelUtils.java
@@ -2,12 +2,24 @@ package com.seven.excel;
 
 import cn.hutool.core.io.resource.ResourceUtil;
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.EasyExcelFactory;
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
+import com.alibaba.excel.metadata.Head;
+import com.alibaba.excel.metadata.data.WriteCellData;
 import com.alibaba.excel.support.ExcelTypeEnum;
+import com.alibaba.excel.write.handler.CellWriteHandler;
+import com.alibaba.excel.write.handler.SheetWriteHandler;
+import com.alibaba.excel.write.merge.LoopMergeStrategy;
+import com.alibaba.excel.write.metadata.holder.WriteSheetHolder;
+import com.alibaba.excel.write.metadata.holder.WriteTableHolder;
+import com.alibaba.excel.write.metadata.holder.WriteWorkbookHolder;
 import com.alibaba.excel.write.metadata.style.WriteCellStyle;
+import com.alibaba.excel.write.metadata.style.WriteFont;
 import com.alibaba.excel.write.style.HorizontalCellStyleStrategy;
 import com.google.common.io.Resources;
+import org.apache.poi.ss.usermodel.*;
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,6 +31,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ExcelUtils {
@@ -87,9 +100,62 @@ public class ExcelUtils {
         }
     }
 
+    @Test
+    public void test() {
+        List<List<String>> head = new ArrayList<>();
+        List<List<String>> data = new ArrayList<>();
+        head.add(Arrays.asList("test", "test"));
+        head.add(Arrays.asList("test", "hhh"));
+        data.add(Arrays.asList("qwe", "asd"));
+        data.add(Arrays.asList("qwe", "hoa"));
+        EasyExcelFactory.write("src/main/java/com/seven/excel/test.xlsx")
+                .sheet(0)
+                .registerWriteHandler(new LoopMergeStrategy(2,0))
+                .registerWriteHandler(horizontalCellStyleStrategy())
+                .head(head)
+                .doWrite(data);
+    }
+
     private HorizontalCellStyleStrategy horizontalCellStyleStrategy() {
-        WriteCellStyle writeCellStyle = new WriteCellStyle();
-        writeCellStyle.setWrapped(true);
-        return new HorizontalCellStyleStrategy(null, writeCellStyle);
+        // 头的策略
+        WriteCellStyle headWriteCellStyle = new WriteCellStyle();
+        // 设置对齐（左对齐）
+        //headWriteCellStyle.setHorizontalAlignment(HorizontalAlignment.LEFT);
+        // 设置对齐（居中对齐）
+        headWriteCellStyle.setHorizontalAlignment(HorizontalAlignment.CENTER);
+        // 背景色, 设置为白色，也是默认颜色
+        headWriteCellStyle.setFillForegroundColor(IndexedColors.WHITE.getIndex());
+        // 字体
+        WriteFont headWriteFont = new WriteFont();
+        headWriteFont.setFontHeightInPoints((short) 12);
+        headWriteCellStyle.setWriteFont(headWriteFont);
+
+        // 内容的策略
+        WriteCellStyle contentWriteCellStyle = new WriteCellStyle();
+        // 这里需要指定 FillPatternType 为FillPatternType.SOLID_FOREGROUND 不然无法显示背景颜色.头默认了 FillPatternType所以可以不指定
+        contentWriteCellStyle.setFillPatternType(FillPatternType.SOLID_FOREGROUND);
+        contentWriteCellStyle.setFillPatternType(FillPatternType.SOLID_FOREGROUND);
+
+        // 背景绿色
+        contentWriteCellStyle.setFillForegroundColor(IndexedColors.GREEN.getIndex());
+        // 字体策略
+        WriteFont contentWriteFont = new WriteFont();
+        contentWriteFont.setFontHeightInPoints((short) 12);
+        contentWriteCellStyle.setWriteFont(contentWriteFont);
+        //设置 自动换行
+        contentWriteCellStyle.setWrapped(true);
+        //设置 垂直居中
+        contentWriteCellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+        //设置 水平居中
+        contentWriteCellStyle.setHorizontalAlignment(HorizontalAlignment.CENTER);
+        //设置边框样式
+        contentWriteCellStyle.setBorderLeft(BorderStyle.THIN);
+        contentWriteCellStyle.setBorderTop(BorderStyle.THIN);
+        contentWriteCellStyle.setBorderRight(BorderStyle.THIN);
+        contentWriteCellStyle.setBorderBottom(BorderStyle.THIN);
+
+        // 这个策略是 头是头的样式 内容是内容的样式 其他的策略可以自己实现
+        HorizontalCellStyleStrategy horizontalCellStyleStrategy = new HorizontalCellStyleStrategy(headWriteCellStyle, contentWriteCellStyle);
+        return horizontalCellStyleStrategy;
     }
 }
